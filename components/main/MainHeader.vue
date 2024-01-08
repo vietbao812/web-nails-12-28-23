d4e34dcdcabcf01f4dc76c1d25f040c112ec0d46
lemyduynnn <duyenle.843703@gmail.com>
2024-01-08 14:57:05 +0700

    fix header and page booking

--- components/main/MainHeader.vue
+++ components/main/MainHeader.vue
@@ -1,28 +1,63 @@
 <template>
   <header class="header relative z-[9999] py-[33px]">
     <div class="container flex items-center justify-center">
-      <nav class="flex items-center w-full justify-between gap-[50px] py-[10px] pr-[10px] pl-[30px] bg-black rounded-full">
-        <div>
+      <nav class="custom_header_hover flex items-center h-[64px] md:gap-0 gap-[42px] justify-between py-[10px] md:pr-[10px] md:pl-[30px] px-[30px] bg-black rounded-full">
+        <div class="shrink-0">
           <NuxtLink to="/">
-            <img src="/images/logo.png" alt="Logo">
+            <img src="/images/logo.png" alt="Logo" class="object-cover">
           </NuxtLink>
         </div>
-        <ul class="flex items-center gap-[10px]">
-          <li v-for="(nav, index) in headerData.nav" :key="index">
-            <NuxtLink :to="nav.link">
-              {{ nav.text }}
-            </NuxtLink>
-          </li>
-          <div>
-            <NuxtLink :to="headerData.booking.url" class="custom-btn">
-              <span>
-                {{ headerData.booking.text }}
-              </span>
-              <img src="/images/arrow-right-bg-black.png" alt="">
-            </NuxtLink>
-          </div>
-        </ul>
+        <div class="custom_hover_block hidden">
+          <ul class="flex items-center gap-[10px]">
+            <li v-for="(nav, index) in headerData.nav" :key="index" :class="isActiveMenuItem(nav.link) ? 'active' : ''" class="custom_hover_active relative">
+              <div v-if="isActiveMenuItem(nav.link)" class="absolute right-2 w-[6px] h-[6px] bg-secondary rounded-full"></div>
+              <NuxtLink :to="nav.link">
+                {{ nav.text }}
+              </NuxtLink>
+            </li>
+            <div>
+              <NuxtLink :to="headerData.booking.url" class="custom-btn">
+                <span class="whitespace-nowrap">
+                  {{ headerData.booking.text }}
+                </span>
+                <img src="/images/arrow-right-bg-black.png" alt="">
+              </NuxtLink>
+            </div>
+          </ul>
+        </div>
+        <div class="custom_hover_hidden md:block hidden pr-[20px]">
+          <img src="/images/arrow-right-bg-white.png" alt="">
+        </div>
+        <div class="md:hidden block">
+          <img src="/images/icon-menu.png" alt="" class="mx-auto cursor-pointer" @click="isOpen = true">
+        </div>
       </nav>
+      <div class="lg:hidden block">
+        <USlideover v-model="isOpen" class="nav-mobile">
+          <div class="p-4 flex-1 relative">
+            <div class="absolute top-3 right-3">
+              <UIcon name="i-ri-close-line" class="w-[40px] h-[40px] cursor-pointer text-secondary" @click="isOpen = false" />
+            </div>
+            <div>
+              <NuxtLink to="/">
+                <img src="/images/logo.png" alt="Logo" class="object-cover">
+              </NuxtLink>
+            </div>
+            <nav class="py-10">
+              <ul class="flex flex-col">
+                <li v-for="(nav, index) in headerData.nav" :key="index">
+                  <NuxtLink
+                    class="block"
+                    :to="nav.link"
+                  >
+                    {{ nav.text }}
+                  </NuxtLink>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </USlideover>
+      </div>
     </div>
   </header>
 </template>
@@ -30,6 +65,15 @@
 <script lang="ts" setup>
 import headerData from '@/data/header.json';
 
+const isOpen = ref(false)
+const route = useRoute();
+
+watch(() => route.fullPath, () => {
+  isOpen.value = false
+})
+const isActiveMenuItem = (link:any) => {
+  return route.path === link;
+};
 </script>
 
 <style lang="scss" scoped>
@@ -38,8 +82,60 @@ header.header {
     li {
       padding: 12px 27px 13px 28px;
       a {
+        white-space: nowrap;
         font-size: 20px;
         color: #fff;
+        font-weight: 300;
+        text-transform: uppercase;
+      }
+    }
+    .custom_hover_active{
+      &:hover {
+        div {
+          display: block;
+        }
+      }
+    }
+  }
+  @media screen and (min-width: 768px) {
+    .custom_header_hover {
+    overflow: hidden;
+    transition-property:width;
+    transition-duration: 1s;
+    // transition-delay: 100ms;
+    width: 220px;
+    &:hover {
+      width: 100%;
+      .custom_hover_hidden {
+        display: none;
+      }
+      .custom_hover_block {
+        display: flex;
+      }
+    }
+  }
+  }
+
+}
+</style>
+
+<style lang="scss">
+.nav-mobile {
+  z-index: 99999;
+  & > div {
+    &:last-child {
+      background: #000;
+      max-width: 70%;
+    }
+  }
+  ul {
+    li {
+      padding: 12px 5px;
+      a {
+        color: #FFF;
+        font-size: 20px;
+        font-weight: 300;
+        text-transform: uppercase;
       }
     }
   }
--- components/section/SectionBooking.vue
+++ components/section/SectionBooking.vue
@@ -1,7 +1,7 @@
 <template>
   <section :id="block.id" :style="{ background: block.background, color: block.text_color }" class="pt-[45px]" :data-cms-bind="dataBinding">
     <div class="container">
-      <div class="grid sm:grid-cols-2 gap-[32px] pb-[45px]">
+      <div class="hidden sm:grid sm:grid-cols-2 gap-[32px] pb-[45px]">
         <div class="flex items-center gap-[30px]">
           <div>
             <h2 class="text-[32px] font-semibold leading-[38px]">
@@ -13,27 +13,76 @@
           </div>
           <div class="w-[160px] h-[1px] bg-white" />
         </div>
-        <div class="hidden sm:flex items-center gap-[30px]">
+        <div class="flex items-center gap-[30px]">
           <h3 class="text-[32px] font-semibold leading-[38px]">
-            {{ block.appointment }}
+            {{ block.appointment.title }}
           </h3>
           <div class="w-[160px] h-[1px] bg-white" />
         </div>
       </div>
       <div>
-        <UForm :validate="validate" :state="state" class="space-y-4" @submit="onSubmit">
-          <UFormGroup label="Email" name="email">
-            <UInput v-model="state.email" />
-          </UFormGroup>
-
-          <UFormGroup label="Password" name="password">
-            <UInput v-model="state.password" type="password" />
-          </UFormGroup>
-
-          <UButton type="submit">
-            Submit
-          </UButton>
-        </UForm>
+        <div class="text-secondary grid sm:grid-cols-2 grid-cols-1 gap-8 h-full">
+          <div class="flex flex-col gap-2">
+            <h4 class="sm:hidden block font-medium text-xl mb-2 uppercase">{{ block.booking.title_mobile }}</h4>
+            <div class="flex gap-2">
+              <input type="date" class="custom-input border-none p-2 text-secondary bg-[#0E0E0E] rounded"/>
+              <USelect v-model="country" :options="countries" id="custom-select"/>
+            </div>
+            <div class="w-full">
+              <USelect v-model="country" :options="countries" id="custom-select"/>
+            </div>
+            <div class="flex flex-col gap-2 text-xs">
+              <div class="flex gap-4 justify-start items-center">
+                <h4>{{ block.booking.label_time.morning }}</h4>
+                <div class="h-px w-20 bg-secondary"></div>
+              </div>
+              <div class="flex gap-4">
+                <button v-for="(item, index) in listTimeMorning" :key="index" class="transition hover:opacity-80 hover:bg-main hover:transition-all h-full border-[0.5px] text-secondary rounded p-2">{{ item }}</button>
+              </div>
+            </div>
+            <div class="flex flex-col gap-2 text-xs">
+              <div class="flex gap-4 justify-start items-center">
+                <h4>{{ block.booking.label_time.afternoon }}</h4>
+                <div class="h-px w-20 bg-secondary"></div>
+              </div>
+              <div class="grid grid-cols-4 gap-2">
+                <button v-for="(item, index) in listTimeAfternoon" :key="index" class="transition hover:opacity-80 hover:bg-main hover:transition-all h-full border-[0.5px] text-secondary rounded p-2">{{ item }}</button>
+              </div>
+            </div>
+          </div>
+          <div class="flex flex-col gap-[20px]">
+            <h4 class="sm:hidden block font-medium text-xl mb-2 uppercase">{{ block.appointment.title_mobile }}</h4>
+            <div class="flex flex-col gap-[19px]">
+              <div class="flex flex-col gap-[19px]">
+                <div class="rounded-full bg-secondary py-[15px] px-[50px] h-[60px] w-2/3">
+                  <p class="text-anchor text-[24px] h-full">Thursday 10-26-2023</p>
+                </div>
+                <div class="w-full">
+                  <p class="text-secondary text-[24px] h-full">Any person</p>
+                </div>
+                <div class="w-full">
+                  <p class="text-secondary text-[20px] h-full">Organic Lemongrass + Green Tea ($100)</p>
+                </div>
+              </div>
+              <div class="flex flex-col h-full w-full gap-[19px] text-xs">
+                <UForm :validate="validate" :state="state" class="space-y-4" @submit="onSubmit">
+                  <UFormGroup name="name">
+                    <UInput v-model="state.name" variant="none" placeholder="Your Name ( Required )" class="custom_input rounded-full"/>
+                  </UFormGroup>
+                  <UFormGroup name="phone">
+                    <UInput v-model="state.phone" variant="none" type="phone" placeholder="Your Phone ( Required )" class="custom_input rounded-full"/>
+                  </UFormGroup>
+                  <UFormGroup name="note">
+                    <UTextarea v-model="state.note" variant="none" type="note" placeholder="Note ( Optional ) (Max length 200 character)" class="custom_input rounded-[30px]"/>
+                  </UFormGroup>
+                  <UButton type="submit" class="flex justify-center w-full text-center transition hover:opacity-80 hover:bg-main hover:transition-all border-[0.5px] text-secondary rounded-full sm:py-[19px] py-[10px]">
+                    {{block.appointment.button}}
+                  </UButton>
+                </UForm>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
@@ -49,15 +98,30 @@ interface Props {
 
 defineProps<Props>()
 
+const countries = ['Eyelash + Full Body Waxing', 'Waxing + Full Body Waxing ($100)', 'Nail Art + Full Body Waxing ($100)'];
+const country = ref(countries[0]);
+
+const listTimeMorning: string[] = [];
+for (let i = 1; i < 5; i++) {
+  listTimeMorning.push(`09:${i}0 am`);
+}
+
+const listTimeAfternoon: string[] = [];
+for (let i = 12; i < 20; i++) {
+  listTimeAfternoon.push(`${i}:00 am`);
+}
+
 const state = reactive({
-  email: undefined,
-  password: undefined
+  name: undefined,
+  phone: undefined,
+  note: undefined
 })
 
 const validate = (state: any): FormError[] => {
   const errors = []
-  if (!state.email) { errors.push({ path: 'email', message: 'Required' }) }
-  if (!state.password) { errors.push({ path: 'password', message: 'Required' }) }
+  if (!state.name) { errors.push({ path: 'name', message: 'Required' }) }
+  if (!state.phone) { errors.push({ path: 'phone', message: 'Required' }) }
+  if (!state.note) { errors.push({ path: 'note', message: 'Required' }) }
   return errors
 }
 
@@ -75,4 +139,18 @@ function onSubmit (event: FormSubmitEvent<any>) {
 //     background-color: var(--color-anchor);
 //   }
 // }
+.custom_input {
+    color: rgba(0, 0, 0, 0.20);
+    background: #FFF;
+    padding: 10px 11.5px;
+
+  }
+
+
+@media screen and (min-width: 640px) {
+  .custom_input {
+    color: rgba(255, 255, 255, 0.20);
+    background: rgba(255, 255, 255, 0.10);
+  }
+}
 </style>
